686dbc36d8334980b92748a7b3e3d448440115e3
Caliaro <caliaro.alexandre@gmail.com>
2021-07-23 10:02:19 +0200

    Fix update article

--- WebContent/WEB-INF/encheres/enchereModeVente/enchereNonCommencee.jsp
+++ WebContent/WEB-INF/encheres/enchereModeVente/enchereNonCommencee.jsp
@@ -35,7 +35,21 @@
 	</div>
 </c:if>
 
-<h1 class="text-center">${null != id ? "Votre vente" : "Nouvelle vente"}</h1>
+<h1 class="text-center">
+	<c:choose>
+		<c:when test="${isVendeurArticle && null != id}">
+			Votre vente
+		</c:when>
+		
+		<c:when test="${!isVendeurArticle && null != id}">
+			Vente
+		</c:when>
+		
+		<c:otherwise>
+			Nouvelle vente
+		</c:otherwise>
+	</c:choose>
+</h1>
 
 <div class="row mt-5">
 	<div class="col-lg-3 d-none d-md-block">
@@ -47,21 +61,21 @@
 			<div class="form-group row">
 				<label for="article" class="col-sm-3 col-form-label">Article :</label>
 				<div class="col-sm-8">
-					<input type="text" class="form-control" id="article" name="article" placeholder="Nom de l'article" value="${article}">
+					<input type="text" class="form-control" id="article" name="article" placeholder="Nom de l'article" value="${article}" ${!isVendeurArticle ? 'readonly' : null}>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="description" class="col-sm-3 col-form-label">Description :</label>
 				<div class="col-sm-8">
-					<textarea rows="4" class="form-control" id="description" name="description">${description}</textarea>
+					<textarea rows="4" class="form-control" id="description" name="description" ${!isVendeurArticle ? 'readonly' : null}>${description}</textarea>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="categorie" class="col-sm-3 col-form-label">Catégorie :</label>
 				<div class="col-sm-8">
-					<select id="categorie" class="form-control" name="categorie">
+					<select id="categorie" class="form-control" name="categorie" ${!isVendeurArticle ? 'readonly' : null}>
 						<option value="1" ${categorie.equals('1') ? 'selected' : ''}>Informatique</option>
 						<option value="2" ${categorie.equals('2') ? 'selected' : ''}>Ameubleument</option>
 						<option value="3" ${categorie.equals('3') ? 'selected' : ''}>Vetement</option>
@@ -74,31 +88,33 @@
 				<img class="col-12" alt="Photo de l'article en ventes" src="images/noPics.jpg">
 			</div>
 
-			<div class="form-group row">
-				<label for="photoArticle" class="col-sm-3 col-form-label">Photo de l'article :</label>
-				<div class="col-sm-8">
-					<input type="file" class="form-control-file" id="photoArticle" name="photoArticle">
+			<c:if test="isVendeurArticle">
+				<div class="form-group row">
+					<label for="photoArticle" class="col-sm-3 col-form-label">Photo de l'article :</label>
+					<div class="col-sm-8">
+						<input type="file" class="form-control-file" id="photoArticle" name="photoArticle">
+					</div>
 				</div>
-			</div>
+			</c:if>
 
 			<div class="form-group row">
 				<label for="miseAPrix" class="col-sm-3 col-form-label">Mise à prix :</label>
 				<div class="col-sm-8">
-					<input type="number" class="form-control" id="miseAPrix" name="miseAPrix" value="${miseAPrix}">
+					<input type="number" class="form-control" id="miseAPrix" name="miseAPrix" value="${miseAPrix}" ${!isVendeurArticle ? 'readonly' : null}>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="debutEnchere" class="col-sm-3 col-form-label">Début de l'enchere :</label>
 				<div class="col-sm-8">
-					<input type="date" class="form-control" id="debutEnchere" name="debutEnchere" value="${debut}">
+					<input type="date" class="form-control" id="debutEnchere" name="debutEnchere" value="${debut}" ${!isVendeurArticle ? 'readonly' : null}>
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<label for="finEnchere" class="col-sm-3 col-form-label">Fin de l'enchere :</label>
 				<div class="col-sm-8">
-					<input type="date" class="form-control" id="finEnchere" name="finEnchere" value="${fin}">
+					<input type="date" class="form-control" id="finEnchere" name="finEnchere" value="${fin}" ${!isVendeurArticle ? 'readonly' : null}>
 				</div>
 			</div>
 
@@ -108,31 +124,33 @@
 				<div class="form-group row">
 					<label for="rue" class="col-sm-3 col-form-label">Rue :</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" id="rue" name="rue" value="${rue}">
+						<input type="text" class="form-control" id="rue" name="rue" value="${rue}" ${!isVendeurArticle ? 'readonly' : null}>
 					</div>
 				</div>
 
 				<div class="form-group row">
 					<label for="codePostal" class="col-sm-3 col-form-label">Code postal :</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" id="codePostal" name="codePostal" value="${codePostal}">
+						<input type="text" class="form-control" id="codePostal" name="codePostal" value="${codePostal}" ${!isVendeurArticle ? 'readonly' : null}>
 					</div>
 				</div>
 
 				<div class="form-group row">
 					<label for="ville" class="col-sm-3 col-form-label">Ville :</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" id="ville" name="ville" value="${ville}">
+						<input type="text" class="form-control" id="ville" name="ville" value="${ville}" ${!isVendeurArticle ? 'readonly' : null}>
 					</div>
 				</div>
 			</fieldset>
 
 			<input type="hidden" id="noArticle" name="noArticle" value="${id}">
 
-			<button type="submit" name="submitButton" value="add" class="btn btn-lg btn-outline-dark mr-5">Enregistrer</button>
-			<button type="submit" name="submitButton" value="cancel" class="btn btn-lg btn-outline-secondary">Annuler</button>
-			<c:if test="${isCancellable}">
-				<button type="submit" name="submitButton" value="delete" class="btn btn-lg btn-outline-danger ml-3">Annuler la vente</button>
+			<c:if test="${isVendeurArticle}">
+				<button type="submit" name="submitButton" value="add" class="btn btn-lg btn-outline-dark mr-5">Enregistrer</button>
+				<button type="submit" name="submitButton" value="cancel" class="btn btn-lg btn-outline-secondary">Annuler</button>
+				<c:if test="${isCancellable}">
+					<button type="submit" name="submitButton" value="delete" class="btn btn-lg btn-outline-danger ml-3">Annuler la vente</button>
+				</c:if>
 			</c:if>
 		</form>
 	</div>
--- src/fr/eni/encheres/ihm/ServletEnchereModeVente.java
+++ src/fr/eni/encheres/ihm/ServletEnchereModeVente.java
@@ -2,9 +2,6 @@ package fr.eni.encheres.ihm;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -65,13 +62,14 @@ public class ServletEnchereModeVente extends HttpServlet {
 						Object noUtilisateur = session.getAttribute("noUtilisateur");
 						request.setAttribute("noUtilisateur", noUtilisateur);
 						request.setAttribute("id", articleIdToString);
+						request.setAttribute("isVendeurArticle", article.getVendeur().getNoUtilisateur() == userId);
 						request.setAttribute("article", article.getNomArticle());
 						request.setAttribute("description", article.getDescription());
 						request.setAttribute("categorie", String.valueOf(article.getCategorie().getNoCategorie()));
 						request.setAttribute("miseAPrix", String.valueOf(article.getMiseAPrix()));
 						request.setAttribute("debut", article.getDateDebutEncheres().toString());
 						request.setAttribute("fin", article.getDateFinEncheres().toString());
-						request.setAttribute("isCancellable", article.getDateDebutEncheres().isAfter(LocalDate.now()) && user.noUtilisateur == article.getVendeur().getNoUtilisateur());
+						request.setAttribute("isCancellable", article.getDateDebutEncheres().isAfter(LocalDate.now()));
 						if (null != retrait) {
 							request.setAttribute("rue", retrait.getRue());
 							request.setAttribute("codePostal", String.valueOf(retrait.getCodePostal()));
@@ -119,82 +117,84 @@ public class ServletEnchereModeVente extends HttpServlet {
 			article = new Article();
 			retrait = new Retrait();
 		}
-					
-		// On verifie l'action qu'on veux effectuer: suppression, ajout ou modification.
-		if (isDeleteButton && articleAlreadyExist) {
-			// Suppression de l'article
-			if (LocalDate.now().isBefore(article.getDateDebutEncheres())) {
-				try {
-					rm.remove(Integer.parseInt(requestNoArticle));
-					am.remove(Integer.parseInt(requestNoArticle));
-					request.setAttribute("deleteSuccess", true);
-					request.setAttribute("alertClass", "info");
-				} catch (NumberFormatException | DALException e) {
-					e.printStackTrace();
-				}
-			}
-		} else {
-			// Dans le cas du clic sur le boutton "Annuler", le traitement est terminé.
-			if (!isCancelButton) {
-				article.setNomArticle(request.getParameter("article"));
-				article.setDescription(request.getParameter("description"));
-				
-				if (!request.getParameter("miseAPrix").equals("")) {
-					article.setMiseAPrix(Integer.parseInt(request.getParameter("miseAPrix")));	
-				}
-
-				categorie.setNoCategorie(Integer.parseInt(request.getParameter("categorie")));
-				article.setCategorie(categorie);
-				
-				String debutEnchere = request.getParameter("debutEnchere");
-				if (!debutEnchere.equals("")) {
-					article.setDateDebutEncheres(LocalDate.parse(debutEnchere));
-				}
-
-				String finEnchere = request.getParameter("finEnchere");
-				if (!finEnchere.equals("")) {
-					article.setDateFinEncheres(LocalDate.parse(finEnchere));
-				}
-
-				if (articleAlreadyExist) {
-					retrait.setNoArticle(noArticle);
-				}
-
-				retrait.setRue(request.getParameter("rue"));
-				retrait.setCodePostal(request.getParameter("codePostal"));
-				retrait.setVille(request.getParameter("ville"));
-				
-				article.setRetrait(retrait);
-				article.setVendeur(user);
-			}
-			
-			try {
-				if (isCancelButton) {
-					request.setAttribute("cancelSuccess", true);
-					request.setAttribute("alertClass", "warning");
-				} else {
-					request.setAttribute("alertClass", "success");
-					if (null != article.getNoArticle()) {
-						am.update(article);
-						rm.update(retrait);
-						request.setAttribute("updateSuccess", true);
-					} else {
-						noArticle = am.add(article);
-						retrait.setNoArticle(noArticle);
-						rm.add(retrait);
-						request.setAttribute("addSuccess", true);
+		
+		if (!articleAlreadyExist || user.getNoUtilisateur() == article.getVendeur().getNoUtilisateur()) {
+			// On verifie l'action qu'on veux effectuer: suppression, ajout ou modification.
+			if (isDeleteButton && articleAlreadyExist) {
+				// Suppression de l'article
+				if (LocalDate.now().isBefore(article.getDateDebutEncheres())) {
+					try {
+						rm.remove(Integer.parseInt(requestNoArticle));
+						am.remove(Integer.parseInt(requestNoArticle));
+						request.setAttribute("deleteSuccess", true);
+						request.setAttribute("alertClass", "info");
+					} catch (NumberFormatException | DALException e) {
+						e.printStackTrace();
 					}
 				}
-			} catch (BusinessException e) {
-				String[] errorMessages = e.getErrorMessages();
-				request.setAttribute("alertClass", "danger");
-				request.setAttribute("errorMessages", errorMessages);
-			} catch (DALException e) {
-				e.printStackTrace();
-			}
+			} else {
+				// Dans le cas du clic sur le boutton "Annuler", le traitement est terminé.
+				if (!isCancelButton) {
+					article.setNomArticle(request.getParameter("article"));
+					article.setDescription(request.getParameter("description"));
+					
+					if (!request.getParameter("miseAPrix").equals("")) {
+						article.setMiseAPrix(Integer.parseInt(request.getParameter("miseAPrix")));	
+					}
 
-			if (null != noArticle) {
-				request.setAttribute("noArticle", noArticle);
+					categorie.setNoCategorie(Integer.parseInt(request.getParameter("categorie")));
+					article.setCategorie(categorie);
+					
+					String debutEnchere = request.getParameter("debutEnchere");
+					if (!debutEnchere.equals("")) {
+						article.setDateDebutEncheres(LocalDate.parse(debutEnchere));
+					}
+
+					String finEnchere = request.getParameter("finEnchere");
+					if (!finEnchere.equals("")) {
+						article.setDateFinEncheres(LocalDate.parse(finEnchere));
+					}
+
+					if (articleAlreadyExist) {
+						retrait.setNoArticle(noArticle);
+					}
+
+					retrait.setRue(request.getParameter("rue"));
+					retrait.setCodePostal(request.getParameter("codePostal"));
+					retrait.setVille(request.getParameter("ville"));
+					
+					article.setRetrait(retrait);
+					article.setVendeur(user);
+				}
+				
+				try {
+					if (isCancelButton) {
+						request.setAttribute("cancelSuccess", true);
+						request.setAttribute("alertClass", "warning");
+					} else {
+						request.setAttribute("alertClass", "success");
+						if (null != article.getNoArticle()) {
+							am.update(article);
+							rm.update(retrait);
+							request.setAttribute("updateSuccess", true);
+						} else {
+							noArticle = am.add(article);
+							retrait.setNoArticle(noArticle);
+							rm.add(retrait);
+							request.setAttribute("addSuccess", true);
+						}
+					}
+				} catch (BusinessException e) {
+					String[] errorMessages = e.getErrorMessages();
+					request.setAttribute("alertClass", "danger");
+					request.setAttribute("errorMessages", errorMessages);
+				} catch (DALException e) {
+					e.printStackTrace();
+				}
+
+				if (null != noArticle) {
+					request.setAttribute("noArticle", noArticle);
+				}	
 			}
 		}
 		
